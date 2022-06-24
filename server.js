f387c90509339d8b305707ab2481a3ae2426f444
Kartikay Dubey <kartikayd2003@gmail.com>
2022-06-24 19:43:55 +0530

    added authentication

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const  { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://heydo:TlsxfCRro7eQagYW@cluster0.qgxvc.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+var crypto = require('crypto');
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = express.Router();
@@ -46,8 +46,61 @@ router.post('/create',(request,response) => {
   )
     
     });
+
+async function checkAuth(urname,pass){
+    const conn = await client.connect();
+    const collection = client.db("heydo").collection("users");
+    const query = {username:urname};
+    const user = await collection.findOne(query);
+    console.log(user);
+    if (user === null) {
+        return null;
+    }
+    const hash = crypto.createHash('sha256').update(`${user.username}${pass}saltnpepper`).digest('hex');
+    console.log(`hash pass ${hash} ${user.password}` );
+    if (hash === user.password) {
+        return true;
+    }
+    else{
+        return false;
+    }
+
+
+
+
+}
+router.post('/authenticate',(req,response)=>{
+    console.log(req.body);
+    const {username ,password} = req.body;
+    console.log(username,password);
+    const auth = checkAuth(username,password).then((res)=>{
+        // console.log(res);
+        response.send({result:res});
+    });
+    // console.log(auth);
+})
+async function insertUser(username,password){
+    const conn = await client.connect();
+    const collection = client.db("heydo").collection("users");
+    const hash = crypto.createHash('sha256').update(`${username}${password}saltnpepper`).digest('hex');
+    const data = { "_id":username,"username":username,"password":password};
+    const result = await collection.insertOne(data);
+    return result;
+
+}
+router.post('/signup',(req,response)=>{
+    const {username,password} = req.body;
+    insertUser(username,password).then((res)=>{
+        console.log(res);
+        response.send({res})
+    }).catch((err)=>{
+        console.log(err);
+        response.send(err);
+    })
+})
 router.get('/',(req,res)=>{
     console.log("invalidURL");
+    
 })
 
     
